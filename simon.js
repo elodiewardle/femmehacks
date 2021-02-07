eadca1eb5383f33a08a2edadaa5caacc46927fd8
Elodie Wardle <ewardle2022@pingry.org>
2021-02-06 20:10:31 -0500

    final!!

--- simon.js
+++ simon.js
@@ -7,17 +7,24 @@ function getOrder(){
     while(numFlashes < score){
         var button = Math.floor(Math.random() * 4) + 1;
         if (button === 1){
-            document.getElementById("button").style.background='#FC4040';
-            await delay(5000);
+            document.getElementById("red").style.background='#FC4040';
+            await delay(3000);
+            document.getElementById("red").style.background='#861F1F';
         } 
         else if (button === 2){
-
+            document.getElementById("blue").style.background='#1947ED;';
+            await delay(3000);
+            document.getElementById("blue").style.background='#182D78';
         }
         else if (button === 3){
-
+            document.getElementById("green").style.background='#3BE31F';
+            await delay(3000);
+            document.getElementById("green").style.background='#145908';
         }
         else{
-
+            document.getElementById("yellow").style.background='#FDF424;';
+            await delay(3000);
+            document.getElementById("yellow").style.background='#6A670F';
         }
     }
 }
